0c88250dd2f2720164fe8a7afe90b2624585a11e
EkaanshArora <ekaansh68@gmail.com>
2022-02-20 22:54:30 +0530

    Initial Commit

---
+++ screencrop/screencropApp.swift
@@ -0,0 +1,17 @@
+//
+//  screencropApp.swift
+//  screencrop
+//
+//  Created by ekaansh arora on 20/02/22.
+//
+
+import SwiftUI
+
+@main
+struct screencropApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
